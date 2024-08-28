2cf60ba847db34593d6012baca85d6f65335b90d
bhattizahid <135619423+bhattizahid@users.noreply.github.com>
2024-08-29 00:28:39 +0500

    New Code

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  viewportWidth: 1366,
+  viewportHeight: 768,
+  e2e: {
+    specPattern: 'cypress/integration/examples/api_testing/*.js',
+    experimentalStudio: true,
+    pageLoadTimeout: 90000,
+    retries: {
+      runMode: 0,
+      openMode: 0,
+    },
+  },
+});
