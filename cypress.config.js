c97d4db00eb47682666b1406c4fd2dbf1ffa4cec
bhattizahid <135619423+bhattizahid@users.noreply.github.com>
2024-08-29 01:27:55 +0500

    Updated

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,27 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  projectId: '7c7fti',
   viewportWidth: 1366,
   viewportHeight: 768,
   e2e: {
-    specPattern: 'cypress/e2e/MYTM Travels/**/*.{js,jsx,ts,tsx}',
+    specPattern: './cypress/e2e/MYTM_Travels/**/*{js,jsx,ts,tsx}',
     experimentalStudio: true,
     pageLoadTimeout: 90000,
     retries: {
       runMode: 0,
       openMode: 0,
+    },
+    setupNodeEvents(on, config) {
+      // Implement node event listeners here
+      // If you have any existing event listeners, include them here
+
+      // Example of an event listener:
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        console.log('Browser is about to launch:', browser.name);
+        // Modify launch options if needed
+        return launchOptions;
+      });
     },
   },
 });
