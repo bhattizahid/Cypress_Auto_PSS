69fa49f8aa891194eac709c2097ae5aa7d68d188
bhattizahid <135619423+bhattizahid@users.noreply.github.com>
2024-08-29 00:58:37 +0500

    New Code

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   viewportWidth: 1366,
   viewportHeight: 768,
   e2e: {
-    specPattern: 'cypress/e2e/api_testing/**/*.{js,jsx,ts,tsx}',
+    specPattern: 'cypress/integration/examples/MYTM Travels/*.js',
     experimentalStudio: true,
     pageLoadTimeout: 90000,
     retries: {
